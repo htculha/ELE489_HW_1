a91b89aae13caf66210a1b393508e2fe28adbb0b
Hüseyin Talha ÇULHA <113991159+htculha@users.noreply.github.com>
2025-03-25 22:15:15 +0300

    Update knn.py

--- knn.py
+++ knn.py
@@ -5,7 +5,10 @@ from sklearn.metrics import accuracy_score, confusion_matrix, classification_rep
 # Euclidean distance fonksiyonu
 def euclidean_distance(x1, x2):
     return np.sqrt(np.sum((x1 - x2) ** 2))
-
+    
+def manhattan_distance(x1, x2):
+    return np.sum(np.abs(x1 - x2))
+    
 # k-NN tahmin fonksiyonu
 def knn_predict(X_train, y_train, x_test, k=3, distance_func=euclidean_distance):
     distances = []
@@ -19,14 +22,12 @@ def knn_predict(X_train, y_train, x_test, k=3, distance_func=euclidean_distance)
     most_common = Counter(k_nearest_labels).most_common(1)
     return most_common[0][0]
 
-
 # Test seti için tüm tahminleri üret
 k = 7
 y_pred = []
 for i in range(len(X_test)):
     prediction = knn_predict(X_train, y_train, X_test.iloc[i], k=k, distance_func=euclidean_distance)
     y_pred.append(prediction)
-
 
 # Doğruluk oranını yazdır
 print("Accuracy:", accuracy_score(y_test, y_pred))
@@ -38,3 +39,16 @@ print(confusion_matrix(y_test, y_pred))
 # Classification Report (precision, recall, f1-score)
 print("\nClassification Report:")
 print(classification_report(y_test, y_pred))
+
+plt.figure(figsize=(9, 5))
+plt.plot(k_values, euclidean_accuracies, marker='o', label='Euclidean', color='purple')
+plt.plot(k_values, manhattan_accuracies, marker='s', label='Manhattan', color='teal')
+
+plt.title("Accuracy vs K (Euclidean vs Manhattan)")
+plt.xlabel("k Value")
+plt.ylabel("Accuracy")
+plt.xticks(k_values)
+plt.grid(True)
+plt.legend()
+plt.show()
+
